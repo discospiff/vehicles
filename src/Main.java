d90142141246ba8efcea9382ba05d5ee9f1d2f64
Brandan <brandan.jones@uc.edu>
2022-08-19 17:51:40 -0400

    Convert program to Object Oriented Design

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,44 @@
-public class Main {
-//    private static int milesPerGallon = 20;
-//    private static double gallonsOfGas = 10.0;
-//    private static int odometer  = 0;
+import java.util.ArrayList;
+import java.util.List;
 
-    private static int[] milesPerGallon = {20, 25, 15};
-    private static double[] gallonsOfGas = {10.0, 8, 12};
-    private static int[] odometer  = {0, 10000, 20000};
+public class Main {
+
+    private static ArrayList<Vehicle> allVehicles = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println("In Vehicles Main");
         System.out.println("Second Line");
+
+
+        Vehicle myVehicle = new Vehicle();
+        myVehicle.setMilesPerGallon(20);
+        myVehicle.setGallonsOfGas(10.0);
+        myVehicle.setOdometer(0);
+        allVehicles.add(myVehicle);
+
+        Vehicle yourVehicle = new Vehicle();
+        yourVehicle.setMilesPerGallon(25);
+        yourVehicle.setGallonsOfGas(8.0);
+        yourVehicle.setOdometer(10000);
+        allVehicles.add(yourVehicle);
+
+        Vehicle ourVehicle = new Vehicle();
+        ourVehicle.setMilesPerGallon(15);
+        ourVehicle.setGallonsOfGas(12.0);
+        ourVehicle.setOdometer(20000);
+        allVehicles.add(ourVehicle);
 
         drive(100);
         drive(60);
     }
 
     private static void drive(int milesDriven) {
-        for (int i = 0; i < 3; i++) {
-            System.out.println(i + " Odometer " + odometer[i] + " Gallons of Gas " + gallonsOfGas[i]);
-            gallonsOfGas[i] = gallonsOfGas[i] - (milesDriven / milesPerGallon[i]);
-            odometer[i] += milesDriven;
-            System.out.println(i + " Odometer " + odometer[i] + " Gallons of Gas " + gallonsOfGas[i]);
+        for (Vehicle vehicle : allVehicles) {
+            System.out.println(" Odometer " + vehicle.getOdometer() + " Gallons of Gas " + vehicle.getGallonsOfGas());
+            vehicle.go(milesDriven);
+            System.out.println(" Odometer " + vehicle.getOdometer() + " Gallons of Gas " + vehicle.getGallonsOfGas());
         }
+
     }
 
 }
